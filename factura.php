b471233d35579dc87c0ea21a93e8769d5468c1e7
Iaonnes <ieoshua12@hotmail.com>
2014-10-02 18:08:58 -0500

    factura php correccion

--- factura.php
+++ factura.php
@@ -7,15 +7,19 @@
 		<?php
 				// declaracion de variables
 				$cantidad1 = $_POST["cantidad1"];
+				$descripcion1 = $_POST["descripcion1"];
 				$precio1 = $_POST["precio1"];
 				$cantidad2 = $_POST["cantidad2"];
+				$descripcion2 = $_POST["descripcion2"];
 				$precio2 = $_POST["precio2"];
 				$cantidad3 = $_POST["cantidad3"];
+				$descripcion3 = $_POST["descripcion3"];
 				$precio3 = $_POST["precio3"];
 				
 				// constantes
 				define('IVA', .16);
 				define("resp", "<br>");
+				define("n", "<br><br>");
 				
 				//importe
 				$mult= $precio1 * $cantidad1;
@@ -28,10 +32,14 @@
 				$total = $subtotal + $iva;
 				
 				echo "La descripcion de la factura es:","<br><br>";
-				echo "Subtotal: ";
-				echo $subtotal, "<br>";
+				echo $cantidad1,"piezas ", $descripcion1," $ ",$precio1.resp;
+				echo $cantidad2,"piezas ", $descripcion2," $ ",$precio2.resp;
+				echo $cantidad3,"piezas ", $descripcion3," $ ",$precio3.n;
+				
+			    echo "Subtotal: ";
+				echo $subtotal.resp;
 				echo "IVA: ";
-				echo $iva,"<br>";
+				echo $iva.resp;
 				echo "TOTAL: ";
 				echo $total;
 				
